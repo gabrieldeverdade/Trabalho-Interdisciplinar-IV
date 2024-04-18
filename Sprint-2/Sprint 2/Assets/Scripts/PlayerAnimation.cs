e4a0ef62bc8f2b26b3806dd32681c2679d071663
Higor Lopes <higorlopes@supplybrain.ai>
2024-04-17 21:51:31 -0300

    adicionado movimentação do personagem

---
+++ Sprint-2/Sprint 2/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction
+{
+	N = 0,
+	NW = 1,
+	W = 2,
+	SW	= 3,
+	S = 4,
+	SE = 5,
+	E = 6,
+	NE = 7
+}
+
+
+public class PlayerAnimation : MonoBehaviour
+{
+	private Animator animator;
+
+	// Start is called before the first frame update
+	void Awake()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	public void SetDirection(Vector2 direction)
+	{
+		var directionArray = direction.magnitude < 0.01 ? "Static" : "Run";
+		Debug.Log($"{directionArray} {GetDirection(direction)}");
+		animator.Play($"{directionArray} {GetDirection(direction)}");
+	}
+
+	string GetDirection(Vector2 direction)
+	{
+		float step = 360 / 8;
+		float offset = step / 2;
+		float angle = Vector2.SignedAngle(Vector2.up, direction.normalized);
+
+		angle += offset;
+		if (angle < 0) angle += 360;
+
+		float stepCount = angle / step;
+
+		return Enum.GetName(typeof(Direction), Mathf.FloorToInt(stepCount));
+	}
+
+}
---
+++ Sprint-2/Sprint 2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	private Rigidbody2D rb;
+	private float moveH, moveV;
+	[SerializeField] private float moveSpeed = 1.0f;
+
+	// Start is called before the first frame update
+	void Start()
+	{ 
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		moveH = Input.GetAxis("Horizontal") * moveSpeed;
+		moveV = Input.GetAxis("Vertical") * moveSpeed;
+		rb.velocity = new Vector2 (moveH, moveV);
+
+		Vector2 direction = new Vector2(moveH, moveV);
+		FindObjectOfType<PlayerAnimation>().SetDirection(direction);
+	}
+}
