8192d459b60d3dea9740aeb7d403a0297be0713f
Higor Lopes <higorlopes@supplybrain.ai>
2024-04-18 21:35:42 -0300

    adicionado colis~ao

--- Sprint-2/Sprint 2/Assets/MosquitoeMovement.cs
+++ Sprint-2/Sprint 2/Assets/MosquitoeMovement.cs
@@ -5,6 +5,7 @@ public class MosquitoeMovement : MonoBehaviour
 	private Rigidbody2D rb;
 	private float moveH, moveV;
 	[SerializeField] private float moveSpeed = 1.0f;
+	[SerializeField] private Rigidbody2D Player;
 
 	// Start is called before the first frame update
 	void Start()
@@ -15,8 +16,8 @@ public class MosquitoeMovement : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		moveH = Random.Range(-1, 1) * moveSpeed;
-		moveV = Random.Range(-1, 1) * moveSpeed;
+		moveH = Random.Range(-100, 100) * moveSpeed;
+		moveV = Random.Range(-100, 100) * moveSpeed;
 		rb.velocity = new Vector2(moveH, moveV);
 	}
 }
